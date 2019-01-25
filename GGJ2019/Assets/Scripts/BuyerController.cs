631958bfe8420fd60c9163ba9a2172c633c657c8
Philipp Helsper <phelsper@gmail.com>
2019-01-26 00:57:28 +0100

    player movement
    player movement and ai based on navmesh

---
+++ GGJ2019/Assets/Scripts/BuyerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BuyerController : MonoBehaviour {
+
+    public Camera _cam;
+    public NavMeshAgent _agent;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // move our agent
+                _agent.SetDestination(hit.point);
+            }
+
+        }
+	}
+}
