9f8e19895bcd064a16fcfed0fbfeacc74695be6a
Philipp Helsper <phelsper@gmail.com>
2019-01-27 08:40:47 +0100

    first repairable object

--- GGJ2019/Assets/Scripts/HouseObject.cs
+++ GGJ2019/Assets/Scripts/HouseObject.cs
@@ -33,10 +33,8 @@ public class HouseObject: MonoBehaviour {
         _wasJudged = false;
         _repairedPercent = 0;
         colliderBounds = gameObject.GetComponent<Collider>().bounds;
-        //viewCone = GameObject.FindGameObjectWithTag("viewCone");
-        //viewPlayer = GameObject.FindGameObjectWithTag("viewPlayer");
 
-        _repairBarInstance = Instantiate(_repairBarPrefab, new Vector3(transform.position.x, transform.position.y+1, transform.position.z), transform.rotation) as GameObject;
+        _repairBarInstance = Instantiate(_repairBarPrefab, new Vector3(transform.position.x, transform.position.y+5, transform.position.z), transform.rotation) as GameObject;
         Debug.Log(_repairBarInstance);
         _repairBarSlider = _repairBarInstance.GetComponentInChildren<Slider>();
         Debug.Log(_repairBarSlider);
@@ -55,6 +53,15 @@ public class HouseObject: MonoBehaviour {
         if(!isRepaired()) {
             if(colliderBounds.Intersects(_viewPlayer.GetComponent<Collider>().bounds)) {
                 _repairedPercent += (_repairProgressPerSecond * Time.deltaTime);
+
+                if (_brokenModel || _fixedModel)
+                {
+                    if (_repairedPercent > 0.8)
+                    {
+                        _brokenModel.active = false;
+                        _fixedModel.active = true;
+                    }
+                }
             }
         }
         _repairBarSlider.value = Mathf.Min(1, _repairedPercent);
